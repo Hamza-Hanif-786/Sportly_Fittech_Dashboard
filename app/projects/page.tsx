e1c57c8ae47f64bd8d528ccc4f91c53f35b60fa7
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-07 09:54:24 +0500

    feat: add pages for blogs, messages, projects, services, and subscribers

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <div>projects</div>
+  )
+}
+
+export default pages
---
+++ app/subscribers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <div>subscribers</div>
+  )
+}
+
+export default pages
