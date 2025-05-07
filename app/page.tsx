3dec4cde8af88bd1e3dce05218a60c6d1c0db493
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-07 09:53:16 +0500

    feat: update grid layout to include an additional column and enhance icon display

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 w-full bg-biscuit rounded-4xl">
+    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4 w-full bg-biscuit rounded-4xl">
       <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
         <Icon icon="mage:dashboard" className="text-gray-500" height={40} width={40} />
       </div>
@@ -11,6 +11,9 @@ export default function Home() {
       </div>
       <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
         <Icon icon="mage:users" className="text-gray-500" height={40} width={40}/>
+      </div>
+      <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
+        <Icon icon="mage:settings" className="text-gray-500" height={40} width={40}/>
       </div>
     </div>
   );
