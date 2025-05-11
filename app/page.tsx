553bd59f995d69faf56bf858aa4e6c2d848de955
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-11 10:18:56 +0500

    feat: restructure Home component layout for improved readability and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,21 @@ import { Icon } from '@iconify/react';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4 w-full bg-biscuit rounded-4xl">
-      <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-        <Icon icon="mage:dashboard" className="text-gray-500" height={40} width={40} />
-      </div>
-      <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-        <Icon icon="mage:inbox" className="text-gray-500" height={40} width={40} />
-      </div>
-      <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-        <Icon icon="mage:users" className="text-gray-500" height={40} width={40}/>
-      </div>
-      <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-        <Icon icon="mage:settings" className="text-gray-500" height={40} width={40}/>
+    <div className="flex flex-col gap-4 p-4 w-full bg-biscuit rounded-4xl shadow">
+      <h1 className="text-2xl font-bold text-center">Welcome to the Admin Dashboard</h1>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
+          <Icon icon="mage:dashboard" className="text-gray-500" height={40} width={40} />
+        </div>
+        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
+          <Icon icon="mage:inbox" className="text-gray-500" height={40} width={40} />
+        </div>
+        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
+          <Icon icon="mage:users" className="text-gray-500" height={40} width={40}/>
+        </div>
+        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
+          <Icon icon="mage:settings" className="text-gray-500" height={40} width={40}/>
+        </div>
       </div>
     </div>
   );
