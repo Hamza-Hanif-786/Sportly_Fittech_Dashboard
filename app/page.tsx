663548bf8273f95b5b0fc7e1396ee57d320476fc
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-12 10:06:51 +0500

    feat: simplify Home component layout and update Dashboard title styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
-import { Icon } from '@iconify/react';
+import StatsSection from '@/components/StatsSection';
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 p-4 w-full bg-biscuit rounded-4xl shadow">
-      <h1 className="text-2xl font-bold text-center">Welcome to the Admin Dashboard</h1>
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
-        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-          <Icon icon="mage:dashboard" className="text-gray-500" height={40} width={40} />
-        </div>
-        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-          <Icon icon="mage:inbox" className="text-gray-500" height={40} width={40} />
-        </div>
-        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-          <Icon icon="mage:users" className="text-gray-500" height={40} width={40}/>
-        </div>
-        <div className="flex items-center justify-center h-64 bg-gray-200 rounded-lg">
-          <Icon icon="mage:settings" className="text-gray-500" height={40} width={40}/>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="text-5xl font-bold font-['Satoshi']">
+        Dashboard
+      </h1>
+      <StatsSection />
+    </>
   );
 }
