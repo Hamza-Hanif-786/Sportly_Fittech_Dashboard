7614088faf97e6a997af7ac1ef48022526ff5081
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-12 09:10:36 +0500

    feat: remove Dashboard title from Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,6 @@ const Header = () => {
   return (
     <div className="flex items-center bg-[#ECE5DE] px-4 py-2 gap-2">
         <SidebarTrigger variant="ghost" />
-        <h1 className="text-2xl font-bold font-['Satoshi']">Dashboard</h1>
     </div>
   )
 }
