30506fb6d3085290b1573ff651a11562e1d8dabb
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-17 12:57:20 +0500

    feat: remove sidebar components and refactor layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
 import type { Metadata } from "next";
 import "./globals.css";
-import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
@@ -16,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SidebarProvider>
     <html lang="en">
       <body className='antialiased'>
-        <AppSidebar />
         <div className="flex flex-col bg-[#71717115] w-full">
           <Header />
           <div className="w-full px-7 py-5">
@@ -28,6 +24,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-    </SidebarProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
-import { SidebarTrigger } from './ui/sidebar'
+import logo from '../public/logo.svg'
 
 const Header = () => {
   return (
     <div className="flex items-center bg-[#ECE5DE] px-4 py-2 gap-2">
-        <SidebarTrigger variant="ghost" />
+      <div className="flex items-center gap-2">
+        <Image src={logo} alt="Logo" className="w-28" />
+      </div>
+      <div className="flex-grow"></div>
+      <div className="flex items-center gap-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+        <button className="bg-green-500 text-white px-4 py-2 rounded">Sign Up</button>
+      </div>
     </div>
   )
 }
