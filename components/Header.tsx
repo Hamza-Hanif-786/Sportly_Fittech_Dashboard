96a7ad442c3b12298608feca5554582e7f9a6e78
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-20 20:00:48 +0500

    feat: improve Header layout with responsive adjustments and new search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,12 +49,12 @@ const Header = () => {
   return (
     <>
       <div className="flex bg-white border-b border-b-gray-200">
-        <div className="flex items-center p-5 xl:p-6 border-r-gray-200 border-r">
+        <div className="flex items-center p-4 sm:p-5 xl:p-6 border-r-gray-200 border-r">
           <Image src={Menu_Icon} alt="Menu Icon" className="size-7 text-[#9EA3AE] cursor-pointer" />
         </div>
-        <div className="flex items-center grow py-5 px-4 xl:p-6">
-          <Image src={logo} alt="Logo" className="w-32 h-8" />
-          <Separator className="mx-6" orientation="vertical" />
+        <div className="flex items-center grow p-4 sm:p-5 xl:p-6">
+          <Image src={logo} alt="Logo" className="w-24 sm:w-32 h-8" />
+          <Separator className="mx-4 sm:mx-6  hidden sm:block" orientation="vertical" />
           <div className='relative hidden md:block'>
             <div className='absolute top-1/2 left-4 transform -translate-y-1/2 flex items-center justify-center'>
               <Image src={Search_Icon} alt="Search Icon" className="pointer-events-none" width={20} height={20} />
@@ -66,7 +66,7 @@ const Header = () => {
               onClick={() => setOpen(true)}
             />
           </div>
-          <div className='block md:hidden'>
+          <div className='hidden sm:block md:hidden'>
             <Button
               variant="outline"
               size="icon"
@@ -96,7 +96,7 @@ const Header = () => {
             </DropdownMenu>
           </div>
 
-          <div>
+          <div className='hidden sm:block'>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Avatar className='size-11 cursor-pointer ring-4 ring-[#BBF246]'>
@@ -123,7 +123,7 @@ const Header = () => {
               <SheetTrigger asChild>
                 <Image src={Menu_Icon} alt="Avatar" className="size-7 cursor-pointer" />
               </SheetTrigger>
-              <SheetContent className='w-1/3'>
+              <SheetContent className='w-3/5 sm:w-2/5'>
                 <SheetHeader>
                   <SheetTitle>
                     <Image src={logo} alt="logo" className="w-28" />
@@ -133,9 +133,41 @@ const Header = () => {
                   <Link href='/sporttracker' className="text-[#192126] text-lg font-medium font-['Galano-Grotesque'] leading-normal">Sport Tracker</Link>
                   <Link href='/healthtracker' className="text-[#192126] text-lg font-medium font-['Galano-Grotesque'] leading-normal">Health Tracker</Link>
                   <Link href='/challenges' className="text-[#192126] text-lg font-medium font-['Galano-Grotesque'] leading-normal">Challenges</Link>
-                  <Button size='default' className='text-[#192126] text-base bg-[#BBF246] cursor-pointer hover:bg-[#BBF246] focus:bg-[#BBF246] focus-visible:ring-0 active:bg-[#BBF246]'>
-                    Upload Post
-                  </Button>
+                  <Separator className="my-4" />
+                  <div className='relative block sm:hidden'>
+                    <div className='absolute top-1/2 left-4 transform -translate-y-1/2 flex items-center justify-center'>
+                      <Image src={Search_Icon} alt="Search Icon" className="pointer-events-none" width={20} height={20} />
+                    </div>
+                    <Input
+                      readOnly
+                      placeholder="Search"
+                      className="w-full lg:w-[170px] xl:w-[250px] h-10 pl-12 placeholder:text-[#9EA3AE] placeholder:text-base font-normal font-['Galano-Grotesque']"
+                      onClick={() => setOpen(true)}
+                    />
+                  </div>
+                  <div className='flex items-center justify-between gap-4 w-full'>
+                    <div className=' w-full'>
+                      <Button className='w-full text-[#192126] text-base bg-[#BBF246] cursor-pointer hover:bg-[#BBF246] focus:bg-[#BBF246] focus-visible:ring-0 active:bg-[#BBF246]'>
+                        Upload Post
+                      </Button>
+                    </div>
+                    <div className='block sm:hidden'>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Avatar className='size-11 cursor-pointer ring-4 ring-[#BBF246]'>
+                            <AvatarImage src={AvatarPic.src} className='' />
+                            <AvatarFallback>HH</AvatarFallback>
+                          </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56">
+                          <DropdownMenuItem>Profile</DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </div>
+                  </div>
+                  
                 </div>
               </SheetContent>
             </Sheet>
